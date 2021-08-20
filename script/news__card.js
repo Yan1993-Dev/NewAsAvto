b3b6d50b0d05997a9566087e38f5dfa53d0169cc
Yan1993-Dev <66394095+Yan1993-Dev@users.noreply.github.com>
2021-08-20 17:18:01 +0300

    News
    Адаптив страниц новостей и карточки новости

--- script/news__card.js
+++ script/news__card.js
@@ -12,6 +12,17 @@ sliders.forEach((el) => {
         autoplay: {
             delay: 3000,
         },
+		
+		breakpoints: {
+            319: {
+                slidesPerView: 1,
+            },
+            512: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            }},
 		keyboard: {
 			enabled: true
 		},
