40a1b9d63a44b6407c00ed0641280fb295673423
Yan1993-Dev <66394095+Yan1993-Dev@users.noreply.github.com>
2021-12-01 19:44:23 +0300

    Правка страниц и иконки

--- script/paint.js
+++ script/paint.js
@@ -11,3 +11,17 @@ tabBtn.forEach(item => {
         }
     })
 })
+
+let modelTab = document.querySelectorAll('.sub__tabs__item')
+
+modelTab.forEach(item => {
+    item.addEventListener('click', () => {
+        if(item.classList.contains('active')){
+            item.classList.remove('active')
+        } else { modelTab.forEach(tab => {
+            tab.classList.remove('active')
+        })
+            item.classList.add('active')
+        }
+    })
+})
