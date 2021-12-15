a86249b91762ce777a55727476535827d14c4202
Yan1993-Dev <66394095+Yan1993-Dev@users.noreply.github.com>
2021-12-15 19:11:55 +0300

    final

--- script/changeModel.js
+++ script/changeModel.js
@@ -3,17 +3,20 @@ const selModel = document.querySelector('.rate-form__model'),
         'Geely': ['Atlas Pro', 'Coolray', 'GS', 'Tugella'],
         'Haval': ['F7', 'F7X', 'Jolion'],
         'Chery': ['Tiggo 4', 'Tiggo 7 Pro', 'Tiggo 8', 'Tiggo 8 Pro'],
-        'Exeed': ['Exeed TXL', 'Exeed FL'],
+        'Exeed': ['Exeed VX', 'Exeed FL'],
         'Changan': ['CS 35 Plus', 'CS 55', 'CS 75FL'],
+        'Faw': ['X40','X80','T77'],
         'Jac': ['Jac S3', 'Jac J3', 'Jac S7', 'Jac T6', 'Jac S5'],
-        'GWM Pickup': ['Wingle 7']
+        'GWMPickup': ['Wingle 7']
     };
     console.log(selModel)
 
 
-document.querySelector('.rate-form__mark select').addEventListener('change', function (e) {
-    selModel.innerHTML = '<select name="model"><option value="">Выберите модель</option></select>';
-    models[this.value].forEach(val => {
-        selModel.querySelector('select').innerHTML += `<option value="${val}">${val}</option>`;
+    document.querySelector('.rate-form__mark select').addEventListener('change', function (e) {
+
+        selModel.innerHTML = '<label for="input_traid" class="label__traid-in">Выберите модель</label> <select name="model" class="input__traid-in"><option value="" class="input__traid-in">Выберите модель</option></select>';
+        models[this.value].forEach(val => {
+            selModel.querySelector('select').innerHTML += `<option value="${val}">${val}</option>`;
+        });
+    
     });
-});
