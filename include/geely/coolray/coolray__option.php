e1adcd6863348ab277b200a699b92c01bfc7a363
Yan <66394095+Yan1993-Dev@users.noreply.github.com>
2022-06-02 17:49:48 +0400

    02.06.22

--- include/geely/coolray/coolray__option.php
+++ include/geely/coolray/coolray__option.php
@@ -1,26 +1,42 @@
-<div class="line__complect">
-            <h2 class="slider_title_three">комфорт и опции</h2>
-        </div>
-        <div class="slider-container_one">
-            <div class="swiper-container_one">
-                <div class="swiper-wrapper_one">
-                    <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/comfort_options_1.jpg" alt="" class="img_slider_one">
-                        <p class="text_slide">Уникальная система управления жестами – изменяй параметры климатической установки, переключай треки мультимедийной системы, регулируй громкость – и все это не прикасаясь к экрану! Настройся на свою волну одним взмахом вместе с CHERY TIGGO 4.</p>
-                    </div>
-                    <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/comfort_options_2.jpg" alt="" class="img_slider_one">
-                        <p class="text_slide">Smart-браслет. Дублирует функции ключа дистанционного управления, делая процесс управления автомобилем максимально комфортным. Браслет может быть подключен к смартфону, что открывает возможности не только мониторинга состояния автомобиля, но и основных показателей здоровья владельца. Непосредственно со Smart-браслета можно дистанционно запустить двигатель.</p>
-                    </div>
-                    <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/comfort_options_3.jpg" alt="" class="img_slider_one">
-                        <p class="text_slide">Мультимедийная система с сенсорным дисплеем 9’ и управлением жестамиТехнологическое превосходство на кончиках пальцев! Уникальные ощущения от контроля обеспечивают 9 сенсорный экран с управлением жестами. Имеется возможность подключить смартфон по Bluetooth или по проводу с возможностью использовать навигацию и другие приложения смартфона. Уникальная система управления жестами – изменяйте параметры климатической установки, переключайте треки, регулируйте громкость – и все это не прикасаясь к экрану! 
-</p>
-                    </div>
-                    <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/comfort_options_4.jpg" alt="" class="img_slider_one">
-                        <p class="text_slide">Chery Tiggo 4 оснащён полным комплектов “теплых опций”: обогрев всех сидений, рулевого колеса, ветрового стекла, боковых зеркал и форсунок стеклоомывателя. Какой бы суровой не оказалась зима, пассажирам салона Tiggo 4 не придется мерзнуть, а водителю не нужно беспокоиться об обледенении стёкол. Помимо этого, автомобиль имеет штатный дистанционный запуск двигателя, управлять которым можно с использованием ключа или smart-браслета.</p>
-                    </div>
-                    <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/comfort_options_5.jpg" alt="" class="img_slider_one">
-                        <p class="text_slide">Камера заднего вида и датчики парковки. Идеальная парковка. Камера заднего вида с направляющими элементами поможет припарковаться в наиболее сложных местах и проконтролирует ваши движения. Ультразвуковые датчики парковки безошибочно обнаружат даже небольшое препятствие на пути.</p>
-                    </div>
+<div class="line_exter">
+    <h2 class="slider_title_one">Экстерьер</h2>
+</div>
+<div class="slider-container_one">
+    <div class="swiper-container_one">
+        <div class="swiper-wrapper_one">
+            <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/exeedLXOne.jpg" alt="" class="img_slider_one">
+            <div class="swiper__wrapper__text">
+            <div class="swiper__inner__wrp">
+                    <p class="swiper__inner__text">Стиль EXEED</p>
                 </div>
-                <div class="swiper-pagination_one"></div>
+            </div>
+                <p class="text_slide">Обладая компактными габаритами, EXEED LX вобрал в себя все самые совеменные технологичные решения. При первом же взгляде на LX, угадывается корпортавная стилистика.</p>
+            </div>
+            <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/exeedLXTwo.jpg" alt="" class="img_slider_one">
+            <div class="swiper__wrapper__text">
+            <div class="swiper__inner__wrp">
+                    <p class="swiper__inner__text">Философия EXEED</p>
+                </div>
+            </div>
+                <p class="text_slide">Спортивный стиль EXEED LX подчеркивает эффектный силуэт и яркие элементы внешней отделки. Совершенство образа намекает на то, что LX рожден для большего - Born For More!</p>
+            </div>
+            <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/exeedLXThr.jpg" alt="" class="img_slider_one">
+            <div class="swiper__wrapper__text">
+            <div class="swiper__inner__wrp">
+                    <p class="swiper__inner__text">Образ EXEED</p>
+                </div>
+            </div>
+                <p class="text_slide">Полностью светодиодные фары с четырьмя отдельными высокопроизводительными линзами не только эффективно освещают дорогу, но и являются запоминающимся стилистическим решением.</p>
+            </div>
+            <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/exeedLXFour.jpg" alt="" class="img_slider_one">
+            <div class="swiper__wrapper__text">
+            <div class="swiper__inner__wrp">
+                    <p class="swiper__inner__text">Легкосплавные диски</p>
+                </div>
+            </div>
+                <p class="text_slide">Колесные диски - важный элемент дизайна LX. На выбор доступны двухцветные легкосплавные диски 18 и 19 дюймов.</p>
             </div>
         </div>
+        <div class="swiper-pagination_one swiper-pagination-custom"></div>
+    </div>
+</div>
--- include/geely/coolray/coolray__technologies.php
+++ include/geely/coolray/coolray__technologies.php
@@ -1,25 +1,42 @@
-<div class="line__complect">
-            <h2 class="slider_title_three">комфорт и опции</h2>
-        </div>
-        <div class="slider-container_one">
-            <div class="swiper-container_one">
-                <div class="swiper-wrapper_one">
-                    <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/technologies_1.jpg" alt="" class="img_slider_one">
-                        <p class="text_slide">Высокотехнологичные и энергоэффективные двигатели CHERY TIGGO 4 позволяют одинаково уверенно чувствовать себя и в плотном потоке мегаполиса, и на свободной загородной трассе. Экономичный бензиновый двигатель объемом 1.5 литра (113 л.с.) или «драйвовый» 1.5 TURBO (147 л.с.), а может быть оптимальный 2.0 (122 л.с.)? Выбор за тобой!</p>
-                    </div>
-                    <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/technologies_2.jpg" alt="" class="img_slider_one">
-                        <p class="text_slide">На выбор представлено 3 варианта трансмиссий: 5МКПП, вариатор 7CVT от компании Bosch и робот с двойным сцеплением 6DCT совместной разработки с Getrag. С двигателями 1,5 и 2,0 может сочетаться пятиступенчатая МКПП или вариатор 7CVT, с турбомотором 1,5 агрегатируется роботизированная КПП 6DCT. В механической КПП применяется механизм переключения INA и сцепление LUK от Schaeffler Group. Самоблокирующийся механизм с блокировочной пластиной предотвращает неправильное включение передачи заднего хода. Литая стальная вилка продлевает срок службы трансмиссии. Каждый 4-й автомобиль в мире (включая VW, BMW, Ford) оснащены сцеплениями и механизмами выбора передач Schaeffler, поставщика мирового уровня, с мировым уровнем качества. Применение комплектующих Schaeffler позволяет гарантировать водителю надежную работу трансмиссии и удовольствие от процесса управления. Применение стального ремня Bosch 7-го поколения в вариаторной КПП, подшипников NSK и других современных решений обеспечивают более высокую надежность, более длительный срок службы и низкие затраты на сервис. Chery - первый китайский автопроизводитель, которому удалось разработать собственный вариатор CVT. Вариатор CVT с 7 «квазипередачами», устанавливаемый на Tiggo 4, работает плавно, осуществляет комфортный разгон без рывков и обеспечивает высокую топливную экономичность. CVT отлично сочетается с двигателем и помогает эффективно управлять скоростью даже при движении по крутой горной дороге, снижая нагрузку на тормозную систему. Для Tiggo 4 с двигателем 1.5 л вариатор получил собственные калибровки для максимального соответствия возможностям нового мотора. Роботизированная КПП с двойным “мокрым” сцеплением доступна с самым мощным двигателем 1,5 турбо 147 л/с. Этот тандем обеспечивает отличную динамику разгона, удобство управления тягой и топливную экономичность. Скорость переключения передачи составляет менее 0,2 секунды. Агрегат хорошо зарекомендовал себя на автомобилях таких марок как Ford и Renault </p>
-                    </div>
-                    <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/technologies_3.jpg" alt="" class="img_slider_one">
-                        <p class="text_slide">Передняя подвеска типа McPherson и задняя — независимая многорычажная обеспечивают идеальный баланс между комфортом и управляемостью на различных дорожных покрытиях. Настройкой подвески занимались специалисты Chery совместно с инженерами британской компании LOTUS ENGINEERING.</p>
-                    </div>
-                    <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/technologies_4.jpg" alt="" class="img_slider_one">
-                        <p class="text_slide">Адаптация к российским условиям эксплуатации за счет оцинковки ультрапрочной, термоформованной сталью – оцинкованы все детали (кроме крыши и капота), толщина цинкового покрытия 0,2 мм (двустороннее). Гарантия на кузов 5 лет! *</p>
-                    </div>
-                    <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/technologies_5.jpg" alt="" class="img_slider_one">
-                        <p class="text_slide">CHERY TIGGO 4 построен на модульной платформе T1X, которая является инновационной разработкой международной команды Chery International. Прочность нижней части кузова обеспечивается за счёт применения тройных лонжеронов, которые участвуют в поглощении энергии удара, что делает кузов энергоёмким и малодеформируемым.</p>
-                    </div>
+<div class="line_exter">
+    <h2 class="slider_title_one">Экстерьер</h2>
+</div>
+<div class="slider-container_one">
+    <div class="swiper-container_one">
+        <div class="swiper-wrapper_one">
+            <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/exeedLXOne.jpg" alt="" class="img_slider_one">
+            <div class="swiper__wrapper__text">
+            <div class="swiper__inner__wrp">
+                    <p class="swiper__inner__text">Стиль EXEED</p>
                 </div>
-                <div class="swiper-pagination_one"></div>
+            </div>
+                <p class="text_slide">Обладая компактными габаритами, EXEED LX вобрал в себя все самые совеменные технологичные решения. При первом же взгляде на LX, угадывается корпортавная стилистика.</p>
+            </div>
+            <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/exeedLXTwo.jpg" alt="" class="img_slider_one">
+            <div class="swiper__wrapper__text">
+            <div class="swiper__inner__wrp">
+                    <p class="swiper__inner__text">Философия EXEED</p>
+                </div>
+            </div>
+                <p class="text_slide">Спортивный стиль EXEED LX подчеркивает эффектный силуэт и яркие элементы внешней отделки. Совершенство образа намекает на то, что LX рожден для большего - Born For More!</p>
+            </div>
+            <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/exeedLXThr.jpg" alt="" class="img_slider_one">
+            <div class="swiper__wrapper__text">
+            <div class="swiper__inner__wrp">
+                    <p class="swiper__inner__text">Образ EXEED</p>
+                </div>
+            </div>
+                <p class="text_slide">Полностью светодиодные фары с четырьмя отдельными высокопроизводительными линзами не только эффективно освещают дорогу, но и являются запоминающимся стилистическим решением.</p>
+            </div>
+            <div class="swiper-slide_one card_one"><img src="<?= SITE_TEMPLATE_PATH ?>/static/images/exeedLXFour.jpg" alt="" class="img_slider_one">
+            <div class="swiper__wrapper__text">
+            <div class="swiper__inner__wrp">
+                    <p class="swiper__inner__text">Легкосплавные диски</p>
+                </div>
+            </div>
+                <p class="text_slide">Колесные диски - важный элемент дизайна LX. На выбор доступны двухцветные легкосплавные диски 18 и 19 дюймов.</p>
             </div>
         </div>
+        <div class="swiper-pagination_one swiper-pagination-custom"></div>
+    </div>
+</div>
